fe1af71dfa473d0c10e0b2fc600130220c49127a
YunSeojin <tkaltk123@gmail.com>
2022-11-01 20:31:47 +0900

    init

---
+++ src/main/java/com/yunseojin/attoassignment/AttoAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.yunseojin.attoassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttoAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttoAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunseojin/attoassignment/AttoAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunseojin.attoassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttoAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
