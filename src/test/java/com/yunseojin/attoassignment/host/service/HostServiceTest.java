c0ec29528d1390b55810c2e694a18e2f56769846
YunSeojin <tkaltk123@gmail.com>
2022-11-05 19:31:38 +0900

    테스트코드 작성 완료

--- src/test/java/com/yunseojin/attoassignment/host/service/HostServiceTest.java
+++ src/test/java/com/yunseojin/attoassignment/host/service/HostServiceTest.java
@@ -1,6 +1,8 @@
 package com.yunseojin.attoassignment.host.service;
 
 import com.yunseojin.attoassignment.etc.enums.ErrorMessage;
+import com.yunseojin.attoassignment.etc.exception.RequestInputException;
+import com.yunseojin.attoassignment.host.entity.HostEntity;
 import com.yunseojin.attoassignment.host.mapper.HostMapper;
 import com.yunseojin.attoassignment.host.serviceimpl.HostServiceImpl;
 import com.yunseojin.attoassignment.host.serviceimpl.InternalHostServiceImpl;
@@ -11,9 +13,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.yunseojin.attoassignment.TestUtil.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -95,57 +99,283 @@ class HostServiceTest {
         });
     }
 
-
+    @Order(10)
+    @DisplayName("updateHost 성공")
     @Test
     void updateHost_success() {
+
+        //given
+        var host = getHostEntity("test", "127.0.0.1");
+        var request = getHostRequest("test2", "192.168.0.1");
+
+        //when
+        when(internalHostService.getHostById(1L)).thenReturn(host);
+        var response = hostService.updateHost(1L, request);
+
+        //then
+        assertEquals(request.getName(), response.getName());
+        assertEquals(request.getIp(), response.getIp());
     }
 
+    @Order(11)
+    @DisplayName("updateHost 호스트 없음")
     @Test
     void updateHost_throwException_hostNotExists() {
+
+        //given
+        var host = getHostEntity("test", "127.0.0.1");
+        var request = getHostRequest("test2", "192.168.0.1");
+
+        //when
+        when(internalHostService.getHostById(2L)).thenThrow(new RequestInputException((ErrorMessage.HOST_NOT_FOUND_EXCEPTION)));
+
+        //then
+        assertError(ErrorMessage.HOST_NOT_FOUND_EXCEPTION, () -> {
+            hostService.updateHost(2L, request);
+        });
     }
 
+    @Order(12)
+    @DisplayName("updateHost 호스트 이름 중복")
     @Test
     void updateHost_throwException_nameDuplicated() {
+
+        //given
+        var host = getHostEntity("test", "127.0.0.1");
+        var request = getHostRequest("test3", "192.168.0.1");
+
+        //when
+        when(internalHostService.getHostById(1L)).thenReturn(host);
+        when(internalHostService.isDuplicateName("test3")).thenReturn(true);
+
+        //then
+        assertError(ErrorMessage.DUPLICATED_HOST_NAME_EXCEPTION, () -> {
+            hostService.updateHost(1L, request);
+        });
     }
 
+    @Order(13)
+    @DisplayName("updateHost 호스트 IP 중복")
     @Test
     void updateHost_throwException_ipDuplicated() {
+
+        //given
+        var host = getHostEntity("test", "127.0.0.1");
+        var request = getHostRequest("test2", "192.168.0.2");
+
+        //when
+        when(internalHostService.getHostById(1L)).thenReturn(host);
+        when(internalHostService.isDuplicateIp("192.168.0.2")).thenReturn(true);
+
+        //then
+        assertError(ErrorMessage.DUPLICATED_HOST_IP_EXCEPTION, () -> {
+            hostService.updateHost(1L, request);
+        });
     }
 
+    @Order(14)
+    @DisplayName("updateHost 이름 변경이 없을 경우 성공")
+    @Test
+    void updateHost_success_nameNotChanged() {
+
+        //given
+        var host = getHostEntity("test", "127.0.0.1");
+        var request = getHostRequest("test", "192.168.0.1");
+
+        //when
+        when(internalHostService.getHostById(1L)).thenReturn(host);
+        var response = hostService.updateHost(1L, request);
+
+        //then
+        assertEquals(request.getName(), response.getName());
+        assertEquals(request.getIp(), response.getIp());
+    }
+
+    @Order(15)
+    @DisplayName("updateHost IP 변경이 없을 경우 성공")
+    @Test
+    void updateHost_success_ipNotChanged() {
+
+        //given
+        var host = getHostEntity("test", "127.0.0.1");
+        var request = getHostRequest("test2", "127.0.0.1");
+
+        //when
+        when(internalHostService.getHostById(1L)).thenReturn(host);
+        var response = hostService.updateHost(1L, request);
+
+        //then
+        assertEquals(request.getName(), response.getName());
+        assertEquals(request.getIp(), response.getIp());
+    }
+
+    @Order(20)
+    @DisplayName("deleteHost 성공")
     @Test
     void deleteHost_success() {
+
+        //given
+        var host = getHostEntity("test", "127.0.0.1");
+
+        //when
+        when(internalHostService.getHostById(1L)).thenReturn(host);
+        hostService.deleteHost(1L);
+
+        //then
+        verify(internalHostService, atMostOnce()).deleteHost(host);
     }
 
+    @Order(21)
+    @DisplayName("deleteHost 호스트 없음")
     @Test
     void deleteHost_throwException_hostNotExists() {
+
+        //given
+        var host = getHostEntity("test", "127.0.0.1");
+
+        //when
+        when(internalHostService.getHostById(2L)).thenThrow(new RequestInputException((ErrorMessage.HOST_NOT_FOUND_EXCEPTION)));
+        
+        //then
+        assertError(ErrorMessage.HOST_NOT_FOUND_EXCEPTION, () -> {
+            hostService.deleteHost(2L);
+        });
     }
 
+    @Order(30)
+    @DisplayName("isAliveHost True")
     @Test
-    void isAliveHost_true() {
+    void isAliveHost_true_ip127_0_0_1() {
+
+        //given
+        var host = getHostEntity("test", "127.0.0.1");
+
+        //when
+        when(internalHostService.getHostById(1L)).thenReturn(host);
+        var result = hostService.isAliveHost(1L);
+
+        //then
+        assertTrue(result);
     }
 
+    @Order(31)
+    @DisplayName("isAliveHost False")
     @Test
-    void isAliveHost_false() {
+    void isAliveHost_false_ip0_0_0_0() {
+
+        //given
+        var host = getHostEntity("test", "0.0.0.0");
+
+        //when
+        when(internalHostService.getHostById(1L)).thenReturn(host);
+        var result = hostService.isAliveHost(1L);
+
+        //then
+        assertFalse(result);
     }
 
+    @Order(32)
+    @DisplayName("isAliveHost 호스트 없음")
     @Test
     void isAliveHost_hostNotExists() {
+
+        //given
+        var host = getHostEntity("test", "127.0.0.1");
+
+        //when
+        when(internalHostService.getHostById(2L)).thenThrow(new RequestInputException((ErrorMessage.HOST_NOT_FOUND_EXCEPTION)));
+
+        //then
+        assertError(ErrorMessage.HOST_NOT_FOUND_EXCEPTION, () -> {
+            hostService.isAliveHost(2L);
+        });
     }
 
+    @Order(40)
+    @DisplayName("getHost 연결상태")
     @Test
-    void getHost_success() {
+    void getHost_isAliveTrue_ip127_0_0_1() {
+
+        //given
+        var host = getHostEntity("test", "127.0.0.1");
+
+        //when
+        when(internalHostService.getHostById(1L)).thenReturn(host);
+        var response = hostService.getHost(1L);
+
+        //then
+        assertTrue(response.getIsAlive());
     }
 
+    @Order(41)
+    @DisplayName("getHost 미연결상태")
+    @Test
+    void getHost_isAliveFalse_ip0_0_0_0() {
+
+        //given
+        var host = getHostEntity("test", "0.0.0.0");
+
+        //when
+        when(internalHostService.getHostById(1L)).thenReturn(host);
+        var response = hostService.getHost(1L);
+
+        //then
+        assertFalse(response.getIsAlive());
+    }
+
+    @Order(42)
+    @DisplayName("getHost 호스트 없음")
     @Test
     void getHost_hostNotExists() {
+
+        //given
+        var host = getHostEntity("test", "127.0.0.1");
+
+        //when
+        when(internalHostService.getHostById(2L)).thenThrow(new RequestInputException((ErrorMessage.HOST_NOT_FOUND_EXCEPTION)));
+
+        //then
+        assertError(ErrorMessage.HOST_NOT_FOUND_EXCEPTION, () -> {
+            hostService.getHost(2L);
+        });
     }
 
+    @Order(50)
+    @DisplayName("getAllHosts 성공")
     @Test
     void getAllHosts_success() {
+
+        //given
+        var list = new ArrayList<HostEntity>();
+        list.add(getHostEntity("test1", "192.168.0.2"));
+        list.add(getHostEntity("test1", "192.168.0.3"));
+        list.add(getHostEntity("test1", "192.168.0.4"));
+
+        //when
+        when(internalHostService.getAllHosts()).thenReturn(list);
+        var result = hostService.getAllHosts();
+
+        //then
+        assertEquals(3, result.size());
     }
 
+    @Order(51)
+    @DisplayName("getAllHosts 호스트가 100개 일 때 1초안에 성공")
     @Test
     void getAllHosts_successLessThen1000ms_hostCountEquals100() {
+
+        //given
+        var list = getTestHosts();
+
+        //when
+        when(internalHostService.getAllHosts()).thenReturn(list);
+        long time = System.nanoTime();
+        var result = hostService.getAllHosts();
+        time = System.nanoTime() - time;
+
+        //then
+        assertTrue(time < 1000000000L);
+        assertEquals(100, result.size());
     }
 
     private void createHostRepeat100() {
@@ -155,5 +385,14 @@ class HostServiceTest {
             var request = getHostRequest("test" + i, "192.168.0." + i + 2);
             hostService.createHost(request);
         }
+    }
+
+    private List<HostEntity> getTestHosts() {
+
+        var list = new ArrayList<HostEntity>();
+        for (int i = 0; i < 100; ++i) {
+            list.add(getHostEntity("test" + i, "0.0.0" + i));
+        }
+        return list;
     }
 }
